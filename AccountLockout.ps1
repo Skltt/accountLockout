6c7149928662e9eea48b0c060a0fb0855a15711d
Zach Young <zyoung@autotrakk.com>
2023-07-21 13:40:14 -0400

    initial commit

---
+++ AccountLockout.ps1
@@ -0,0 +1,12 @@
+﻿$AccountLockOutEvent = Get-EventLog -LogName "Security" -InstanceID 4740 -Newest 1
+$LockedAccount = $($AccountLockOutEvent.ReplacementStrings[0])
+$AccountLockOutEventTime = $AccountLockOutEvent.TimeGenerated
+$AccountLockOutEventMessage = $AccountLockOutEvent.Message
+$messageParameters = @{
+Subject = "Account Locked Out: $LockedAccount"
+Body = "Account $LockedAccount was locked out on $AccountLockOutEventTime.`n`nEvent Details:`n`n$AccountLockOutEventMessage"
+From = "no-reply@autotrakk.com"
+To = "zyoung@autotrakk.com","mcowden@autotrakk.com"
+SmtpServer = "mailrelay.autotrakk.com"
+}
+Send-MailMessage @messageParameters
